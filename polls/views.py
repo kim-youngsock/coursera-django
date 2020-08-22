efa0a45a27b403cb0eaced9eb1b2cc59168f9769
Ameya Shahu <ameyashahu@gmail.com>
2020-08-22 12:36:21 +0530

    polls.views.owner response updated

--- polls/views.py
+++ polls/views.py
@@ -47,4 +47,4 @@ def vote(request, question_id):
 
 
 def owner(request):
-    return HttpResponse("Hello, world. 943b5fd1 is the polls owner.")
+    return HttpResponse("Hello, world. 1340ccf is the polls owner.")
