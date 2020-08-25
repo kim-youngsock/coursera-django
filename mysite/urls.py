5b6ad8f8d22987d65760bd567ebee205ec6d15ee
Ameya Shahu <ameyashahu@gmail.com>
2020-08-25 13:36:53 +0530

    autos CRUD added

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,8 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home/main.html')),
     path('hello/', include('hello.urls')),
     path('polls/', include('polls.urls')),
+    path('autos/', include('autos.urls')),
+
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
